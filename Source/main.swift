6b9c87ccda17f23f263e7cf643dbce7a8b75c316
Damien <damien.morard@etu.unige.ch>
2017-04-04 15:28:34 +0200

    Class natural

--- Source/main.swift
+++ Source/main.swift
@@ -1,2 +1,6 @@
 let greet = "hello!"
 print(greet)
+
+let test = Nat.succ(x:Nat.zero)
+
+print(test)
--- Source/natural.swift
+++ Source/natural.swift
@@ -1,20 +1,26 @@
 import LogicKit
 
-//Generator
-let zero = Value("0")
 
-func succ(x: Term) -> Map {
-  return ["succ": x]
+
+class Nat{
+
+  //Generator
+  static let zero = Value("0")
+
+  static func succ(x: Term) -> Map {
+    return ["succ": x]
+  }
+
 }
 
-succ(x: zero)
+//print(lol)
 
-func add(x: Term, y: Term) ->
+//func add(x: Term, y: Term) ->
 
-func is_even(what: Term) -> Goal {
+/*func is_even(what: Term) -> Goal {
     return (what === zero) ||
            delayed (fresh { x in
              what === succ(x:succ(x:x)) &&
              is_even(what:x)
            })
-}
+}*/
