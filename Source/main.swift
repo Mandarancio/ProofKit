5eb7cabb82bdf2f92c8f672802e4012a501cec8b
Martino Ferrari <manda.mgf@gmail.com>
2017-04-04 18:06:47 +0200

    First implementation of Nat

--- Source/main.swift
+++ Source/main.swift
@@ -1,6 +1,13 @@
+import LogicKit
+
 let greet = "hello!"
 print(greet)
 
-let test = Nat.succ(x:Nat.zero())
-
-print(test.is_even())
+let x = Variable(named:"x")
+let goal = Nat.is_even (y: x)
+for substitution in solve (goal) {
+     print ("substitution found")
+     for (_, value) in substitution.reified() {
+         print("* \(value)")
+     }
+}
--- Source/natural.swift
+++ Source/natural.swift
@@ -1,64 +1,23 @@
 import LogicKit
 
 
-public struct Nat: Term{
-
-  fileprivate let value: Int
-
-  private init(_ val: Int) {
-    self.value = val
-  }
-
-  public func getValue() -> Int{
-    return self.value
-  }
-
+public struct Nat{
   //Generator
-  static public func zero() -> Nat{
-    return Nat(0)
+  static public func zero() -> Value<Int>{
+    return Value(0)
   }
 
-  static public func succ(x: Nat) -> Nat {
-    return Nat(x.getValue()+1)
+  static public func succ(x: Term) -> Map {
+    return ["succ": x]
   }
 
   //Modifieur
-  public func is_even() -> Goal{
-    return (self === Nat.zero()) ||
+  static  public  func is_even(y: Term) -> Goal{
+    return (y === Nat.zero()) ||
             delayed(fresh {x in
-              self === Nat.succ(x:Nat.succ(x:x)) &&
-              x.is_even()
+              y === Nat.succ(x:Nat.succ(x:x)) &&
+              Nat.is_even(y:x)
             })
   }
 
-  // public func is_even() -> Bool {
-  //
-  //   if x.getValue() == 0 {
-  //     return true
-  //   }
-  //   else{
-  //     return !self.is_even(x:dec(x:x))
-  //   }
-  // }
-
-  public func equals(_ other: Term) -> Bool {
-      if let rhs = (other as? Nat) {
-          return rhs.value == self.value
-      }
-      return false
-  }
-
-
 }
-
-//print(lol)
-
-//func add(x: Term, y: Term) ->
-
-/*func is_even(what: Term) -> Goal {
-    return (what === zero) ||
-           delayed (fresh { x in
-             what === succ(x:succ(x:x)) &&
-             is_even(what:x)
-           })
-}*/
