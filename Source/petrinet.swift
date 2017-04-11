e4c941884b289a5f6e47387d1307d9db72e50f49
Florian Cabot <florian.cabot@etu.unige.ch>
2017-04-11 16:15:41 +0200

    Rewriting of Petri Net generators

--- Source/petrinet.swift
+++ Source/petrinet.swift
@@ -6,12 +6,8 @@ public struct Petrinet {
     return Value<Int>(0)
   }
 
-  static public func add_input(_ place: Term, _ transition: Term, _ weight: Term, _ subnet: Term) -> Term {
-    return Map(["place": place, "transition": transition, "weight": weight, "subnet": subnet])
-  }
-
-  static public func add_output(_ transition: Term, _ place: Term, _ weight: Term, _ subnet: Term) -> Term {
-    return Map(["transition": transition, "place": place, "weight": weight, "subnet": subnet])
+  static public func add_edge(_ place: Term, _ transition: Term, _ weight: Term, _ isInput: Term, _ subnet: Term) -> Term {
+    return Map(["place": place, "transition": transition, "weight": weight, "isInput" : isInput, "subnet": subnet])
   }
 
   //Modifiers
@@ -26,7 +22,7 @@ public func test_petrinets() {
 
   print("\nPetri Nets tests : \n")
 
-  let net = Petrinet.add_input(Nat.n(1), Nat.n(1), Nat.n(1), Petrinet.empty())
+  let net = Petrinet.add_edge(Nat.n(1), Nat.n(1), Nat.n(1), Boolean.True(), Petrinet.empty())
   let x_net = Variable(named:"x_net")
   let goal_net = Petrinet.is_empty(x_net)
 
