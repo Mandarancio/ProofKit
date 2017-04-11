678fb6d843fbab2dd111f040b5ddb2e72002c96d
Martino Ferrari <manda.mgf@gmail.com>
2017-04-11 15:21:29 +0200

    Added tests

---
+++ Tests/ADTTests/ADTTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import LogicKit
+
+class ADTTests : XCTestCase {
+    func testExample() {
+        print("here")
+    }
+
+
+    static var allTests : [(String, (ADTTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import ADTTests
+
+XCTMain([
+     testCase(ADTTests.allTests),
+])
