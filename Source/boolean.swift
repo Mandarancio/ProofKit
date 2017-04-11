9cf05852d175230f6ab6b267484db6874d2272e3
Martino Ferrari <manda.mgf@gmail.com>
2017-04-11 16:15:54 +0200

    Added boolean adt

---
+++ Source/boolean.swift
@@ -0,0 +1,22 @@
+
+public struct Boolean {
+    public static func True() -> Term{
+      return Value<Bool>(true)
+    }
+
+    public static func False() -> Term{
+      return Value<Bool>(false)
+    }
+
+    public static func toBoolean(_ x: Bool) -> Term{
+      return Value<Bool>(x)
+    }
+
+    public static func isTrue(_ x: Term) -> Goal {
+      return x === Boolean.True()
+    }
+
+    public static func isFalse(_ x: Term) -> Goal{
+      return x === Boolean.False()
+    }
+}
