ddfef436663ea16b0bb903446d446ba56871e5d5
Martino Ferrari <manda.mgf@gmail.com>
2017-04-12 20:30:24 +0200

    Cleaned up code, added Nat.add(...) function

--- Source/natural.swift
+++ Source/natural.swift
@@ -1,24 +1,5 @@
 import LogicKit
-
-fileprivate func add_zero_left(_ x: Term,_ lhs: Term,_ rhs: Term) -> Goal {
-   return Nat.is_nat(lhs) && rhs === Nat.zero()
-}
-
-fileprivate func add_zero_right(_ x: Term,_ lhs: Term,_ rhs: Term) -> Goal {
-   return x === lhs
-}
-
-fileprivate func add_succ_left(_ x: Term,_ lhs: Term,_ rhs: Term) -> Goal {
-   return Nat.is_nat(lhs) && delayed(fresh {y in rhs === Nat.succ(x: y)})
-}
-
-fileprivate func add_succ_right(_ x: Term,_ lhs: Term,_ rhs: Term) -> Goal {
-   return delayed(fresh {
-             y in rhs === Nat.succ(x: y) && delayed(fresh {
-                   z in x === Nat.succ(x: z) && operation(z,lhs,y, Nat.Add.axioms)
-               })
-             })
-}
+import Foundation
 
 public struct Nat{
   //Generator
@@ -41,50 +22,33 @@ public struct Nat{
   	}
     return t
   }
-
-  //Modifieur
-  static  public func is_even(x: Term) -> Goal{
-    return (x === Nat.zero()) ||
-            delayed(fresh {y in
-              x === Nat.succ(x:Nat.succ(x:y)) &&
-              Nat.is_even(x:y)
-            })
-  }
-
-  struct Add {
-    public static let axioms : [[String: (Term, Term, Term) -> Goal]] = [
-      [
-        "left" : add_zero_left,
-        "right": add_zero_right
-      ],
-      [
-        "left" : add_succ_left,
-        "right": add_succ_right
-      ]
-    ]
-  }
-
   static public func is_nat(_ x: Term) -> Goal{
-    // x == 0 || exists y such that (x == s(y) and y is nat)
-  	return (x === Nat.zero() || delayed(fresh {y in x === Nat.succ(x:y) && Nat.is_nat(y)}))
+    return (x === Nat.zero() || delayed(fresh {y in x === Nat.succ(x:y) && Nat.is_nat(y)}))
   }
 
-  static public func count(_ x: Term) -> Int{
+  static public func count(_ x: Term) -> String{
    	if x.equals(Nat.zero()){
-   		return 0
+   		return "0"
    	}
    	if let map = (x as? Map){
-   		return 1+Nat.count(map["succ"]!)
+      let k = Nat.count(map["succ"]!)
+      let f = k.components(separatedBy:"+")
+      if f.count == 1{
+        return String(1+Int(k)!)
+      }
+      if f[0].characters.count == 0 {
+   		   return "1+"+f[1]
+      }
+      return String(1+Int(f[0])!)+"+"+f[1]
    	}
-   	return -1
+    if x is Variable {
+      return "+"+(x as! Variable).description
+    }
+   	return "+?"
   }
 
-}
-
-public func operation(_ z: Term, _ lhs: Term, _ rhs: Term,_ axioms:[[String: (Term, Term, Term) -> Goal]]) -> Goal{
-  var g = (axioms[0]["left"]!(z,lhs,rhs) && axioms[0]["right"]!(z,lhs,rhs))
-  for i in 1...axioms.count-1 {
-    g = g || (axioms[i]["left"]!(z,lhs,rhs) && axioms[i]["right"]!(z,lhs,rhs))
+  public static func add(_ l: Term, _ r: Term) -> Map{
+      let o =  Operator.n(l, r, "N+")
+      return o
   }
-  return g
 }
