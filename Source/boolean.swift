838aa5a2cdb1a2b5881cb2a50e491fdc1875d2bf
Martino Ferrari <manda.mgf@gmail.com>
2017-04-12 20:31:17 +0200

    Added all basic operator for boolean

--- Source/boolean.swift
+++ Source/boolean.swift
@@ -8,7 +8,7 @@ public struct Boolean {
       return Value<Bool>(false)
     }
 
-    public static func toBoolean(_ x: Bool) -> Term{
+    public static func to(_ x: Bool) -> Term{
       return Value<Bool>(x)
     }
 
@@ -18,5 +18,20 @@ public struct Boolean {
 
     public static func isFalse(_ x: Term) -> Goal{
       return x === Boolean.False()
+    }
+
+    public static func not(_ x: Term)->Term{
+      return Map([
+        "name":Value<String>("B!"),
+        "rhs": x
+      ])
+    }
+
+    public static func or(_ lhs: Term, _ rhs: Term)->Term{
+      return Operator.n(lhs,rhs,"B.or")
+    }
+
+    public static func and(_ lhs: Term, _ rhs: Term)->Term{
+      return Operator.n(lhs,rhs,"B.and")
     }
 }
