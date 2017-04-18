a48a68f8eb8131851ae04df394f319d4ac136153
Martino Ferrari <manda.mgf@gmail.com>
2017-04-18 12:59:21 +0200

    Some Test using adt classes

--- Source/main.swift
+++ Source/main.swift
@@ -1,29 +1,14 @@
 import LogicKit
 
-let greet = "hello!"
-print(greet)
-/*
-let x = Nat.n(2)
-let y = Nat.n(3)
-let z = Variable(named:"x")
-print(x)
-let goal = operation(z,x,y, Nat.Add.axioms)
-for substitution in solve (goal) {
-     print ("substitution found")
-     for (t, value) in substitution.reified().prefix(100) {
-     if (t == z){
-         print("* \(t) \(Nat.count(value))")
-       }
-     }
-}
-*/
+var bool = Boolean()
 
+var nat = Nat()
 let x = Variable(named: "x")
-let o = Nat.add(Nat.n(2), Nat.n(1))
+let o = nat.o("+")(Nat.n(2), Nat.n(1))
 print(o)
-let g = axioms["N+"]![1].applay(o,x)
+let g = nat.a("+")[1].applay(o,x)
 for s in solve(g){
   for (t,v) in s.reified().prefix(10){
-      print(" * \(t) \((v))")
+      print(" * \(t) \(v)")
   }
 }
