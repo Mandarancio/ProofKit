993b48be58e6e7486d2f70c5bfb6849d7e991278
Martino Ferrari <manda.mgf@gmail.com>
2017-04-18 12:58:43 +0200

    Added ADT class structure

--- Source/adt.swift
+++ Source/adt.swift
@@ -11,26 +11,81 @@ struct Proof {
   }
 }
 
-public  let axioms : [String: [Rule]] = [
-  "B!": [
-    Rule(Boolean.not(Boolean.False()),Boolean.True()),
-    Rule(Boolean.not(Boolean.True()),Boolean.False())
-  ],
-  "B.or": [
-    Rule(Boolean.or(Boolean.True(),Variable(named:"B.or.0.$1")),Boolean.True()),
-    Rule(Boolean.or(Variable(named:"B.or.1.$1"),Boolean.True()),Boolean.True()),
-    Rule(Boolean.or(Boolean.False(),Boolean.False()),Boolean.False())
-  ],
-  "B.and":[
-    Rule(Boolean.and(Boolean.False(),Variable(named:"B.and.0.$1")),Boolean.False()),
-    Rule(Boolean.and(Variable(named:"B.and.1.$1"),Boolean.True()),Boolean.False()),
-    Rule(Boolean.and(Boolean.True(),Boolean.True()),Boolean.True())
-  ],
-  "N+": [Rule(Nat.add(Variable(named: "N+.0.$1"), Value(0)),
-              Variable(named: "N+.0.$1")),
-         Rule(Nat.add(Variable(named: "N+.1.$1"), Nat.succ(x: Variable(named: "N+.1.$2"))),
-              Nat.succ(x: Nat.add(Variable(named: "N+.1.$1"),Variable(named:"N+.1.$2"))))]
-]
+public class ADT{
+  public let name : String
+  private var _axioms : [String:[Rule]]
+  private var _operators : [String: (Term ...) ->Term]
+  private var _arity : [String: Int]
+  private var _generators : [String :  (Term...)->Term]
+  private var _gen_arity : [String : Int]
+
+  public init(_ name: String){
+    self.name = name
+    self._generators = [:]
+    self._operators = [:]
+    self._axioms = [:]
+    self._arity = [:]
+    self._gen_arity = [:]
+  }
+  
+  public class func belong(_ term: Term ) -> Goal {
+        return Boolean.isTrue(Boolean.True())
+  }
+
+  public func get_axioms(_ name: String) -> [Rule]{
+    return self._axioms[name]!
+  }
+  public func a(_ name: String) -> [Rule] {
+    return self.get_axioms(name)
+  }
+  
+  internal func add_generator(_ name: String, _ generator: @escaping((Term...)->Term), arity: Int = 0){
+    self._generators[name] = generator
+    self._gen_arity[name] = arity
+  }
+  
+  internal func add_operator(_ name: String, _ oper: @escaping((Term...)->Term), _ axioms: [Rule], arity: Int = 2){
+    self._operators[name] = oper
+    self._arity[name] = arity
+    self._axioms[name] = axioms
+  }
+  
+  public func get_operator(_ name: String)-> ((Term...)->Term){
+    return  self._operators[name]!
+  }
+  
+  public func o(_ name: String)-> ((Term...)->Term){
+    return self.get_operator(name)
+  }
+  
+  public func get_generator(_ name: String) -> ((Term...)->Term) {
+    return self._generators[name]!
+  }
+  
+  public func g(_ name: String) -> ((Term...)->Term) {
+    return self.get_generator(name)
+  }
+  
+}
+
+public struct ADTManager{
+  public init(){
+    self.adts = [:]
+  }
+
+  public mutating func add_adt(_ name: String, _ adt : ADT){
+    self.adts[name]=adt
+  }
+
+  public func get_adt(_ name: String) -> ADT?{
+    return self.adts[name]
+  }
+
+  public func get_axioms(adt : String, op : String) -> [Rule]?{
+    return self.adts[adt]?.get_axioms(op)
+  }
+  private var adts : [String:ADT] = [:]
+}
 
 public struct Operator{
   public static func n(_ lhs: Term, _ rhs: Term, _ name: String) -> Map{
