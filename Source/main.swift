c3e8c82a961de61d56747a085e596364d1ebd9e3
Martino Ferrari <manda.mgf@gmail.com>
2017-04-18 13:26:48 +0200

    Test of linked list

--- Source/main.swift
+++ Source/main.swift
@@ -1,12 +1,20 @@
 import LogicKit
 
 var bool = Boolean()
-
 var nat = Nat()
+
+var l = LList.insert(Nat.n(4),LList.insert(Nat.n(2),LList.empty()))
+print(l)
+var g = LList.belong(l)
+for s in solve(g){
+  for (t,v) in s.reified().prefix(10){
+      print(" * \(t) \(v)")
+  }
+}
 let x = Variable(named: "x")
 let o = nat.o("+")(Nat.n(2), Nat.n(1))
 print(o)
-let g = nat.a("+")[1].applay(o,x)
+g = nat.a("+")[1].applay(o,x)
 for s in solve(g){
   for (t,v) in s.reified().prefix(10){
       print(" * \(t) \(v)")
