14266a676991f08c3899bc9a51fe8c2adfc73c03
Martino Ferrari <manda.mgf@gmail.com>
2017-04-18 12:59:11 +0200

    Now extend ADT Class

--- Source/natural.swift
+++ Source/natural.swift
@@ -1,14 +1,26 @@
 import LogicKit
 import Foundation
 
-public struct Nat{
+public class Nat: ADT{
+  public init(){
+    super.init("Nat")
+    self.add_generator("zero", Nat.zero)
+    self.add_generator("false", Nat.succ,arity: 1)
+    self.add_operator("+", Nat.add, [
+      Rule(Nat.add(Variable(named: "+.0.$1"), Value(0)),
+                  Variable(named: "+.0.$1")),
+             Rule(Nat.add(Variable(named: "+.1.$1"), Nat.succ(x: Variable(named: "+.1.$2"))),
+                  Nat.succ(x: Nat.add(Variable(named: "+.1.$1"),Variable(named:"+.1.$2"))))
+    ])
+  }
+  
   //Generator
-  static public func zero() -> Term{
+  static public func zero(_:Term...) -> Term{
     return Value<Int>(0)
   }
 
-  static public func succ(x: Term) -> Term {
-    return Map(["succ": x])
+  static public func succ(x: Term...) -> Term {
+    return Map(["succ": x[0]])
   }
 
   static public func n(_ x: Int) -> Term {
@@ -22,8 +34,8 @@ public struct Nat{
   	}
     return t
   }
-  static public func is_nat(_ x: Term) -> Goal{
-    return (x === Nat.zero() || delayed(fresh {y in x === Nat.succ(x:y) && Nat.is_nat(y)}))
+  public class override func belong(_ x: Term) -> Goal{
+    return (x === Nat.zero() || delayed(fresh {y in x === Nat.succ(x:y) && Nat.belong(y)}))
   }
 
   static public func count(_ x: Term) -> String{
@@ -47,8 +59,8 @@ public struct Nat{
    	return "+?"
   }
 
-  public static func add(_ l: Term, _ r: Term) -> Map{
-      let o =  Operator.n(l, r, "N+")
+  public static func add(_ operands: Term...) -> Term{
+      let o =  Operator.n(operands[0], operands[1], "+")
       return o
   }
 }
