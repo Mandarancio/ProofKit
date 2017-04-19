f5b68ac189df00f79ef55cf2a1681334a0c57fe6
Martino Ferrari <manda.mgf@gmail.com>
2017-04-19 18:44:56 +0200

    Example with new standard and adts

--- Source/main.swift
+++ Source/main.swift
@@ -1,22 +1,41 @@
 import LogicKit
 
-var bool = Boolean()
-var nat = Nat()
+func get_result(_ goal : Goal, _ x : Variable) -> Term{
+  var res: Term = Value("nil")
+  for s in solve(goal){
+    for (t,v) in s.reified().prefix(10){
+        if t.equals(x) {
+          res = v
+        }
+    }
+  }
+  return res
+}
 
-var l = LList.insert(Nat.n(4),LList.insert(Nat.n(2),LList.empty()))
-print(l)
-var g = LList.belong(l)
-for s in solve(g){
-  for (t,v) in s.reified().prefix(10){
-      print(" * \(t) \(v)")
-  }
-}
+let concat_axioms = ADTs["llist"].a("concat")
+print("Linked List: Concatenation")
+print( "axioms:\n 0. \(concat_axioms[0].pprint())\n 1. \(concat_axioms[1].pprint())\n")
+var l0 = LList.insert(Nat.n(1),LList.insert(Nat.n(3),LList.empty()))
+var l1 = LList.insert(Nat.n(5),LList.empty())
+var l2 = LList.concat(l1,l0)
+var res: Term = LList.empty()
 let x = Variable(named: "x")
-let o = nat.o("+")(Nat.n(2), Nat.n(1))
-print(o)
-g = nat.a("+")[1].applay(o,x)
-for s in solve(g){
-  for (t,v) in s.reified().prefix(10){
-      print(" * \(t) \(v)")
-  }
-}
+var g = concat_axioms[1].applay(l2,x)
+
+res = get_result(g,x)
+var str = " \(ADTs.pprint(l2)) => \(ADTs.pprint(res))"
+
+g = ADTs["llist"].a("concat")[0].applay(res,x)
+res = get_result(g,x)
+str += " => \(ADTs.pprint(res))"
+print("Example: ")
+print(str)
+
+let o = ADTs["nat"]["+"](Nat.n(2), Nat.n(1))
+g = ADTs["nat"].a("+")[1].applay(o,x)
+res = get_result(g,x)
+print("\nNatural: Add")
+let add_axioms = ADTs["nat"].a("+")
+print( "axioms:\n 0. \(add_axioms[0].pprint())\n 1. \(add_axioms[1].pprint())\n")
+
+print(" \(ADTs.pprint(o)) => \(ADTs.pprint(res))")
