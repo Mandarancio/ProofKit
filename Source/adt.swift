647cec120e3f924ba383e6a59b1c8341ab5a07d3
Martino Ferrari <manda.mgf@gmail.com>
2017-04-19 18:43:52 +0200

    Added helpers and ADTManager structure

--- Source/adt.swift
+++ Source/adt.swift
@@ -1,5 +1,8 @@
 import LogicKit
+import Foundation
 
+//// SINGLETON ADT Manager
+public let ADTs = ADTManager()
 
 //// namespace containing all the operations to prove an axiom
 struct Proof {
@@ -25,65 +28,129 @@ public class ADT{
     self._arity = [:]
     self._gen_arity = [:]
   }
-  
+
+  subscript ( i : String) -> ((Term...)->Term){
+      get{
+        if _generators[i] != nil{
+          return _generators[i]!
+        }
+        return _operators[i]!
+      }
+      set{
+      }
+  }
+
+  //// GOAL To detect if a term belongs to an ADT
   public class func belong(_ term: Term ) -> Goal {
         return Boolean.isTrue(Boolean.True())
   }
 
+  //// Boolean check if a term belongs to an ADT (used by ADTManager)
+  public func check(_ term: Term) -> Bool{
+    return false
+  }
+
+  //// Function to nicely print a TERM belonging to an ADT
+  public func pprint(_ term: Term) -> String{
+    return ""
+  }
+
+  //// Retrive axioms for an operation
   public func get_axioms(_ name: String) -> [Rule]{
     return self._axioms[name]!
   }
+
+  //// Shortcut to retrive axioms
   public func a(_ name: String) -> [Rule] {
     return self.get_axioms(name)
   }
-  
+
+
+  //// Internal use only
   internal func add_generator(_ name: String, _ generator: @escaping((Term...)->Term), arity: Int = 0){
     self._generators[name] = generator
     self._gen_arity[name] = arity
   }
-  
+  //// Internal use only
   internal func add_operator(_ name: String, _ oper: @escaping((Term...)->Term), _ axioms: [Rule], arity: Int = 2){
     self._operators[name] = oper
     self._arity[name] = arity
     self._axioms[name] = axioms
   }
-  
+
+  //// Retrive operator generator
   public func get_operator(_ name: String)-> ((Term...)->Term){
     return  self._operators[name]!
   }
-  
+
+  //// Shortcut to retrive an operator generator
   public func o(_ name: String)-> ((Term...)->Term){
     return self.get_operator(name)
   }
-  
+
+  //// Retrive adt generator
   public func get_generator(_ name: String) -> ((Term...)->Term) {
     return self._generators[name]!
   }
-  
+
+  //// Shortcut for generator
   public func g(_ name: String) -> ((Term...)->Term) {
     return self.get_generator(name)
   }
-  
+
 }
 
 //// To be used to store all the ADTs
 public struct ADTManager{
-  public init(){
-    self.adts = [:]
+  fileprivate init(){
+    self["nat"] = Nat()
+    self["boolean"] = Boolean()
+    self["llist"] = LList()
   }
 
-  public mutating func add_adt(_ name: String, _ adt : ADT){
-    self.adts[name]=adt
+  subscript ( i : String) -> ADT{
+      get{
+        return adts[i]!
+      }
+      set{
+        adts[i] = newValue
+      }
   }
 
-  public func get_adt(_ name: String) -> ADT?{
-    return self.adts[name]
-  }
-
-  public func get_axioms(adt : String, op : String) -> [Rule]?{
-    return self.adts[adt]?.get_axioms(op)
-  }
   private var adts : [String:ADT] = [:]
+
+  public func pprint(_ term: Term) -> String{
+    if term.equals(Value("nil")){
+      return ""
+    }
+    for (_,adt) in self.adts{
+      if adt.check(term){
+        return adt.pprint(term)
+      }
+    }
+    if let op = (term as? Map){
+      if op["name"] != nil && op["lhs"] != nil && op["rhs"] != nil {
+        let x = (op["name"] as! Value<String>)
+        return self.pprint(op["lhs"]!)+" "+x.description+" "+self.pprint(op["rhs"]!)
+      }
+      return op.description
+    }
+    if let op = (term as? Value<Int>){
+      return op.description
+    }
+    if let op = (term as? Value<String>){
+      return op.description
+    }
+    if let op = (term as? Variable) {
+      let name = op.description
+      let ks = name.components(separatedBy:"$")
+      if ks.count == 2{
+        return "$"+ks[1]
+      }
+      return name
+    }
+    return "?"
+  }
 }
 
 //// Basic form of an operator
@@ -103,9 +170,13 @@ public struct Rule {
     self.lTerm = lT
     self.rTerm = rT
   }
-
+  //// Applay axiom to term t and result in r
   public func applay(_ t: Term, _ r: Term)->Goal{
     return t === self.lTerm && r === self.rTerm
+  }
+  //// Pretty print of the axiom
+  public func pprint() -> String{
+    return ADTs.pprint(self.lTerm)+" = "+ADTs.pprint(self.rTerm)
   }
 
   public let lTerm : Term
