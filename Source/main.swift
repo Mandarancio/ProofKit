79367592c5b6448cd0bb29ef4fc26dbe0408a333
Martino Ferrari <manda.mgf@gmail.com>
2017-04-20 17:18:43 +0200

    Cleaner code and use of universal evaluator to solve operations

--- Source/main.swift
+++ Source/main.swift
@@ -1,35 +1,6 @@
 import LogicKit
 
 
-func get_result(_ goal : Goal, _ x : Variable) -> Term{
-  var res: Term = Value("nil")
-  for s in solve(goal){
-    for (t,v) in s.reified().prefix(10){
-        if t.equals(x) {
-          res = v
-        }
-    }
-  }
-  return res
-}
-
-func resolve(_ op: Term, _ rules: [Rule]) -> Term{
-  let x = Variable(named: "solver.x")
-  var curr = op
-  var res : Term = Value(0)
-  let nilV = Value("nil")
-  while !nilV.equals(res){
-    res = nilV
-    for r in rules{
-      res = get_result(r.applay(curr, x),x)
-      if !nilV.equals(res){
-        curr = res
-        break
-      }
-    }
-  }
-  return curr
-}
 
 func print_ax(_ ax: [Rule]){
   var i = 0
@@ -49,27 +20,27 @@ for o in llist.get_operators(){
 let x = Variable(named: "x")
 var res : Term
 
-let contains = ADTs["llist"].a("contains")
 var k = LList.n([Nat.n(2),Nat.n(5),Nat.n(3), Nat.n(1),Nat.n(4)])
 var exists = ADTs["llist"]["contains"](k,Nat.n(1))
-res = resolve(exists, contains)
-print("\n \(ADTs.pprint(exists)) = \(ADTs.pprint(res))\n")
-
-let concat_axioms = ADTs["llist"].a("concat")
+res = ADTs.eval(exists)//resolve(exists, contains)
+print("\n \(ADTs.pprint(exists)) = \(ADTs.pprint(res))")
 
 var l0 = LList.n([Nat.n(1),Nat.n(3), Nat.n(8)])
 var l1 = LList.n([Nat.n(7),Nat.n(5)])
 var l2 = LList.concat(l1,l0)
 
-res = resolve(l2, concat_axioms)
-var str = " \(ADTs.pprint(l2)) => \(ADTs.pprint(res))"
-print(str)
+res = ADTs.eval(l2)
+print(" \(ADTs.pprint(l2)) => \(ADTs.pprint(res))")
 
-let o = ADTs["nat"]["+"](Nat.n(2), Nat.n(1))
-print("\nNatural: Add")
-let add_axioms = ADTs["nat"].a("+")
-print_ax(add_axioms)
+l0 = LList.size(LList.n([Nat.n(1),Nat.n(3),Nat.n(4)]))
+res = ADTs.eval(l0)
+print(" \(ADTs.pprint(l0)) => \(ADTs.pprint(res))")
 
-res = resolve(o, add_axioms)
+let o = ADTs["nat"]["+"](Nat.n(2), Nat.n(3))
+for i in ADTs["nat"].get_operators(){
+  print("\nat - \(i):")
+  print_ax( ADTs["nat"].a(i))
+}
+res = ADTs.eval(o)
 
-print(" \(ADTs.pprint(o)) => \(ADTs.pprint(res))")
+print("\n \(ADTs.pprint(o)) => \(ADTs.pprint(res))")
