f16abd9d4ef7672a5a8b90058523ae286ea7c6cd
Martino Ferrari <manda.mgf@gmail.com>
2017-04-21 12:20:12 +0200

    Added conditional axioms

--- Source/adt.swift
+++ Source/adt.swift
@@ -21,7 +21,7 @@ public func get_result(_ goal : Goal, _ x : Variable) -> Term{
 func resolve(_ op: Term, _ rules: [Rule]) -> Term{
   let x = Variable(named: "solver.x")
   var curr = op
-  var res : Term = Value(0)
+  var res : Term = op
 
   while !vNil.equals(res){
     res = vNil
@@ -186,6 +186,7 @@ public struct ADTManager{
     self["nat"] = Nat()
     self["boolean"] = Boolean()
     self["llist"] = LList()
+    self["set"] = Set()
   }
 
   subscript ( i : String) -> ADT{
@@ -285,19 +286,28 @@ public struct Operator{
 
 //// Proved tehorem and axioms data struct
 public struct Rule {
-  init(_ lT : Term,_ rT: Term){
+  init(_ lT : Term,_ rT: Term, _ condition: Term = Boolean.True()){
     self.lTerm = lT
     self.rTerm = rT
+    self.condition = condition
   }
   //// Applay axiom to term t and result in r
   public func applay(_ t: Term, _ r: Term)->Goal{
-    return t === self.lTerm && r === self.rTerm
+    let x = Variable(named: "r.x")
+    let c = get_result(condition === x && self.lTerm === t && self.rTerm === r, x)
+    return t === self.lTerm && r === self.rTerm && ADTs.eval(c) === Boolean.True()
   }
+
   //// Pretty print of the axiom
   public func pprint() -> String{
-    return ADTs.pprint(self.lTerm)+" = "+ADTs.pprint(self.rTerm)
+    if condition.equals(Boolean.True()){
+      return "\(ADTs.pprint(self.lTerm)) = \(ADTs.pprint(self.rTerm))"
+    }else{
+      return "if \(ADTs.pprint(self.condition)) then \n\t\(ADTs.pprint(self.lTerm)) = \(ADTs.pprint(self.rTerm))"
+    }
   }
 
   public let lTerm : Term
   public let rTerm : Term
+  public let condition : Term
 }
