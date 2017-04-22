9692185e3b8ed8de67b9ec2d7a0a90f21aa70504
Martino Ferrari <manda.mgf@gmail.com>
2017-04-22 13:32:50 +0200

    Added Sequence ADT

--- Source/listadt.swift
+++ Source/listadt.swift
@@ -276,6 +276,44 @@ public class Sequence : ADT {
     super.init("sequence")
     self.add_generator("empty", Sequence.empty)
     self.add_generator("cons", Sequence.cons, arity:3)
+    self.add_operator("push", Sequence.push,[
+      Rule(
+        Sequence.push(Variable(named:"push.0.$0"), Sequence.empty()),
+        Sequence.cons(Variable(named:"push.0.$0"), Nat.zero(), Sequence.empty())
+      ),
+      Rule(
+        Sequence.push(Variable(named:"push.1.$0"), Sequence.cons(Variable(named:"push.1.$1"), Variable(named:"push.1.$2"), Variable(named:"push.1.$3"))),
+        Sequence.cons(Variable(named:"push.1.$0"), Nat.succ(x:Variable(named:"push.1.$2")), Sequence.cons(Variable(named:"push.1.$1"), Variable(named:"push.1.$2"), Variable(named:"push.1.$3")))
+      )
+    ],arity:2)
+    self.add_operator("get", Sequence.getAt,[
+      Rule(
+        Sequence.getAt(Sequence.empty(), Variable(named: "get.0.$0")),
+        vNil
+      ),
+      Rule(
+        Sequence.getAt(Sequence.cons(Variable(named: "get.1.$0"), Variable(named: "get.1.$1"), Variable(named: "get.1.$2")), Variable(named: "get.1.$1")),
+        Variable(named: "get.1.$0")
+      ),
+      Rule(
+        Sequence.getAt(Sequence.cons(Variable(named: "get.2.$0"), Variable(named: "get.2.$1"), Variable(named: "get.2.$2")), Variable(named: "get.2.$3")),
+        Sequence.getAt(Variable(named: "get.2.$2"), Variable(named: "get.2.$3"))
+      )
+    ], arity:2)
+    self.add_operator("set", Sequence.setAt,[
+      Rule(
+        Sequence.setAt(Sequence.empty(), Variable(named: "set.0.$0"), Variable(named: "set.0.$1")),
+        Sequence.empty()
+      ),
+      Rule(
+        Sequence.setAt(Sequence.cons(Variable(named: "set.1.$0"), Variable(named: "set.1.$1"), Variable(named: "set.1.$2")), Variable(named: "set.1.$1"), Variable(named: "set.1.$3")),
+        Sequence.cons(Variable(named: "set.1.$3"), Variable(named:"set.1.$1"), Variable(named:"set.1.$2"))
+      ),
+      Rule(
+        Sequence.setAt(Sequence.cons(Variable(named: "set.2.$0"), Variable(named: "set.2.$1"), Variable(named: "set.2.$2")), Variable(named: "set.2.$3"), Variable(named: "set.2.$4")),
+        Sequence.cons(Variable(named: "set.2.$0"), Variable(named:"set.2.$1"), Sequence.setAt(Variable(named:"set.2.$2"), Variable(named: "set.2.$3"), Variable(named:"set.2.$4")))
+      )
+    ],arity:3)
   }
 
   public static func empty(_:Term...)->Term{
@@ -290,9 +328,24 @@ public class Sequence : ADT {
     ])
   }
 
-  // public static func insert(_ t: Term...)->Term{
-    // return Operator.n()
-  // }
+  public class func n(_ t: [Term])-> Term{
+    var r = push(t[0], Sequence.empty())
+    for i in 1...t.count-1{
+      r = push(t[i], r)
+    }
+    return r
+  }
+
+  public static func push(_ t: Term...)->Term{
+    return Operator.n("push",t[0],t[1])
+  }
+
+  public static func getAt(_ t: Term...)->Term{
+    return Operator.n("get",t[0],t[1])
+  }
+  public static func setAt(_ t:Term...)->Term{
+    return Operator.n("set",t[0],t[1],t[2])
+  }
 
   public class override func belong(_ x: Term) -> Goal{
     return (x === Sequence.empty() || delayed(fresh {y in fresh{w in fresh{z in x === Sequence.cons(y,w,z) && Sequence.belong(z)}}}))
@@ -311,6 +364,7 @@ public class Sequence : ADT {
           s += "\(ADTs.pprint(m["index"]!)): \(ADTs.pprint(m["value"]!))"
           x = m["rest"]!
         }else{
+          s += ", rest : \(ADTs.pprint(m))"
           x = Sequence.empty()
         }
       }else if let m = (x as? Variable){
