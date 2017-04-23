77eb2ce5a39329283e5fbe9b09b9950b7cba0029
Martino Ferrari <manda.mgf@gmail.com>
2017-04-24 01:33:44 +0200

    Starter working on some unit test

--- Tests/ProofKitLibTests/ProofKitLibTests.swift
+++ Tests/ProofKitLibTests/ProofKitLibTests.swift
@@ -2,25 +2,39 @@ import XCTest
 import LogicKit
 @testable import ProofKitLib
 
-extension ADTTests {
-    static var allTests : [(String, (ADTTests) -> () throws -> Void)] {
+extension ProofKitLibTests {
+    static var allTests : [(String, (ProofKitLibTests) -> () throws -> Void)] {
         return [
+            ("testBool", testBool),
             ("testNat", testNat)
         ]
     }
 }
 
-class ADTTests: XCTestCase {
-//  var adt : ProofKit.ADTManager = ProofKit.ADTs
+class ProofKitLibTests: XCTestCase {
+  let adt : ADTManager = ADTManager.instance()
+
+  internal func TAssert(_ a: Term,_ b: Term){
+    XCTAssertTrue(adt.eval(a).equals(adt.eval(b)))
+  }
+
+  internal func FAssert(_ a: Term,_ b: Term){
+    XCTAssertFalse(adt.eval(a).equals(adt.eval(b)))
+  }
+
 
   override func setUp() {
     super.setUp()
-
    }
+  func testBool(){
 
+  }
   func testNat() {
-  	print(Nat.zero())
-  	//print(Nat.add(Nat.n(1),Nat.n(2)))
-    // XCTAssertEqual(adt.eval(Nat.add(Nat.n(1),Nat.n(1))),Nat.n(2))
+    // 1+1 == 2
+    self.TAssert(Nat.add(Nat.n(1),Nat.n(1)),Nat.n(2))
+    // (1+2)+1 == 1+(2+1)
+    self.TAssert(Nat.add(Nat.add(Nat.n(1),Nat.n(2)),Nat.n(1)), Nat.add(Nat.n(1),Nat.add(Nat.n(2),Nat.n(1))))
+    // (1+3) != (1+2)
+    self.FAssert(Nat.add(Nat.n(1),Nat.n(3)),Nat.add(Nat.n(1),Nat.n(2)))
   }
 }
