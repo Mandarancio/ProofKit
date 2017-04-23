1cdf42d72ceabdadbb3456170331be95e18e1bb9
Martino Ferrari <manda.mgf@gmail.com>
2017-04-24 01:49:57 +0200

    Basic Nat and Boolean operator tests

--- Tests/ProofKitLibTests/ProofKitLibTests.swift
+++ Tests/ProofKitLibTests/ProofKitLibTests.swift
@@ -14,11 +14,15 @@ extension ProofKitLibTests {
 class ProofKitLibTests: XCTestCase {
   let adt : ADTManager = ADTManager.instance()
 
-  internal func TAssert(_ a: Term,_ b: Term, _ msg: String = "a == b is false"){
+  internal func TAssert(_ a: Term,_ b: Term){
+    let msg = "\(adt.pprint(a)) == \(adt.pprint(b))"
+    print(msg)
     XCTAssertTrue(adt.eval(a).equals(adt.eval(b)), msg)
   }
 
-  internal func FAssert(_ a: Term,_ b: Term, _ msg: String = "a != b is false"){
+  internal func FAssert(_ a: Term,_ b: Term){
+    let msg = "\(adt.pprint(a)) != \(adt.pprint(b))"
+    print(msg)
     XCTAssertFalse(adt.eval(a).equals(adt.eval(b)),msg)
   }
 
@@ -28,15 +32,40 @@ class ProofKitLibTests: XCTestCase {
    }
 
   func testBool(){
-
+    // true != false
+    self.FAssert(Boolean.True(),Boolean.False())
+    // not true == false
+    self.TAssert(Boolean.not(Boolean.True()), Boolean.False())
+    // not false == true
+    self.TAssert(Boolean.not(Boolean.False()), Boolean.True())
+    // true and true == true
+    self.TAssert(Boolean.and(Boolean.True(), Boolean.True()),Boolean.True())
+    // true and false == false
+    self.TAssert(Boolean.and(Boolean.True(), Boolean.False()),Boolean.False())
+    // false and true == false
+    self.TAssert(Boolean.and(Boolean.False(), Boolean.True()),Boolean.False())
+    // false and false == false
+    self.TAssert(Boolean.and(Boolean.False(), Boolean.False()),Boolean.False())
+    // true or true == true
+    self.TAssert(Boolean.or(Boolean.True(), Boolean.True()),Boolean.True())
+    // true or false == true
+    self.TAssert(Boolean.or(Boolean.True(), Boolean.False()),Boolean.True())
+    // false or true == true
+    self.TAssert(Boolean.or(Boolean.False(), Boolean.True()),Boolean.True())
+    // false or false == false
+    self.TAssert(Boolean.or(Boolean.False(), Boolean.False()),Boolean.False())
   }
 
   func testNat() {
-    // 1+1 == 2
-    self.TAssert(Nat.add(Nat.n(1),Nat.n(1)),Nat.n(2))
-    // (1+2)+1 == 1+(2+1)
-    self.TAssert(Nat.add(Nat.add(Nat.n(1),Nat.n(2)),Nat.n(1)), Nat.add(Nat.n(1),Nat.add(Nat.n(2),Nat.n(1))))
-    // (1+3) != (1+2)
-    self.FAssert(Nat.add(Nat.n(1),Nat.n(3)),Nat.add(Nat.n(1),Nat.n(2)))
+    // x + 0 == x
+    self.TAssert(Nat.add(Nat.n(5),Nat.zero()),Nat.n(5))
+    // 3 + 2 = 5
+    self.TAssert(Nat.add(Nat.n(3),Nat.n(2)), Nat.n(5))
+    // 3*0 = 0
+    self.TAssert(Nat.mul(Nat.n(3),Nat.zero()),Nat.zero())
+    // 3*1 = 1
+    self.TAssert(Nat.mul(Nat.n(3),Nat.n(1)),Nat.n(3))
+    // 3*2 = 6
+    self.TAssert(Nat.mul(Nat.n(3),Nat.n(2)),Nat.n(6))
   }
 }
