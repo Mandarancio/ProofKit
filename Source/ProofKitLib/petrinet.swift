836906deff7b27817700e982b9d81f8e5e90b009
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-21 20:10:34 +0200

    Implemented "belong" for Petrinet

--- Source/ProofKitLib/petrinet.swift
+++ Source/ProofKitLib/petrinet.swift
@@ -3,7 +3,7 @@ public class Petrinet : ADT {
     public init(){
       super.init("petrinet")
       self.add_generator("null", Petrinet.null)
-      self.add_generator("add_edge", Petrinet.add_edge, arity: 4)
+      self.add_generator("add_edge", Petrinet.add_edge, arity: 5)
     }
 
     public static func null(_:Term...) -> Term{
@@ -11,11 +11,18 @@ public class Petrinet : ADT {
     }
 
     public static func add_edge(_ operands: Term...) -> Term{
-      return Map(["from":operands[0], "to":operands[1], "isInput":operands[2], "weight":operands[3]])
+      return Map(["from":operands[0], "to":operands[1], "isInput":operands[2], "weight":operands[3], "net":operands[4]])
     }
 
     public class override func belong(_ term: Term ) -> Goal {
-          return term === Petrinet.null() //TODO add complex case
+          return term === Petrinet.null() || delayed( freshn {
+            ops in term === Petrinet.add_edge(ops["0"], ops["1"], ops["2"], ops["3"], ops["4"]) 
+              && Nat.belong(ops["0"])
+              && Nat.belong(ops["1"])
+              && Boolean.belong(ops["2"])
+              && Nat.belong(ops["3"])
+              && Petrinet.belong(ops["4"]) 
+          })
     }
 
     public override func pprint(_ term: Term) -> String{
