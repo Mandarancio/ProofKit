6c6756fdee64d3d81021c3c7b3f7afc40692bad6
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-21 19:45:21 +0200

    Base code for developing petrinets

--- Source/ProofKitLib/petrinet.swift
+++ Source/ProofKitLib/petrinet.swift
@@ -1,40 +1,34 @@
 import LogicKit
-
-public struct Petrinet {
-  //Generators
-  static public func empty() -> Term {
-    return Value<Int>(0)
-  }
-
-  static public func add_edge(_ place: Term, _ transition: Term, _ weight: Term, _ isInput: Term, _ subnet: Term) -> Term {
-    return Map(["place": place, "transition": transition, "weight": weight, "isInput" : isInput, "subnet": subnet])
-  }
-
-  //Modifiers
-
-  static public func is_empty(_ x : Term) -> Goal{
-    return x === Petrinet.empty()
-  }
-
-}
-
-public func test_petrinets() {
-
-  print("\nPetri Nets tests : \n")
-
-  let net = Petrinet.add_edge(Nat.n(1), Nat.n(1), Nat.n(1), Boolean.True(), Petrinet.empty())
-  let x_net = Variable(named:"x_net")
-  let goal_net = Petrinet.is_empty(x_net)
-
-  print(net)
-
-  for subs in solve (goal_net) {
-
-    for (t, value) in subs.reified().prefix(100) {
-      if (t == x_net){
-        print("* \(t) \(value)")
-      }
+public class Petrinet : ADT {
+    public init(){
+      super.init("petrinet")
+      self.add_generator("null", Petrinet.null)
+      self.add_generator("add_edge", Petrinet.add_edge, arity: 4)
     }
 
-  }
+    public static func null(_:Term...) -> Term{
+      return Value<Bool>(false)
+    }
+
+    public static func add_edge(_ operands: Term...) -> Term{
+      return Map(["from":operands[0], "to":operands[1], "isInput":operands[2], "weight":operands[3]])
+    }
+
+    public class override func belong(_ term: Term ) -> Goal {
+          return term === Petrinet.null() //TODO add complex case
+    }
+
+    public override func pprint(_ term: Term) -> String{
+      if term.equals(Boolean.True()){
+        return "true"
+      }
+      if term.equals(Boolean.False()){
+        return "false"
+      }
+      return "?"
+    }
+
+    public override func check(_ term: Term) -> Bool{
+      return term.equals(Boolean.False()) || term.equals(Boolean.True())
+    }
 }
