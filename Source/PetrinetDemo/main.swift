a9d5678fda59ff18c21a1ccb1e68eeed07504fae
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-23 01:53:25 +0200

    Added some petri nets examples
    They currently don't work for operators but they do work for
    representation.

---
+++ Source/PetrinetDemo/main.swift
@@ -0,0 +1,28 @@
+import LogicKit
+import ProofKitLib
+
+/*
+
+[.] places
+|.| transitions
+-w->  edge with weight w
+
+
+this example is this net :
+
+[0]-1->|0|<-1-[1]-1->|1|-2->[2]
+   <-2-
+
+*/
+//let adtman_foo = ADTManager.instance()
+
+/*let net = Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(1),
+          Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(-2),
+          Petrinet.add_edge(Nat.n(1), Nat.n(0), Integer.n(1),
+          Petrinet.add_edge(Nat.n(1), Nat.n(1), Integer.n(1),
+          Petrinet.add_edge(Nat.n(2), Nat.n(1), Integer.n(-2),
+          Petrinet.null()
+          )))))
+
+print(Petrinet.to_string(net))
+print(Petrinet.to_matrix(net).to_string())*/
--- Source/ProofKitDemo2/main.swift
+++ Source/ProofKitDemo2/main.swift
@@ -9,3 +9,45 @@ print(adtm.pprint(c))
 let r = adtm.eval(c)
 print(adtm.pprint(r))
 print(r)
+
+/*
+
+[.] places
+|.| transitions
+-w->  edge with weight w
+
+
+this example is this net :
+
+[0]-1->|0|<-1-[1]-1->|1|-2->[2]
+   <-2-
+
+*/
+
+print("\n--------PETRINET---------\n")
+
+let net = Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(1),
+          Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(-2),
+          Petrinet.add_edge(Nat.n(1), Nat.n(0), Integer.n(1),
+          Petrinet.add_edge(Nat.n(1), Nat.n(1), Integer.n(1),
+          Petrinet.add_edge(Nat.n(2), Nat.n(1), Integer.n(-2),
+          Petrinet.null()
+          )))))
+
+print(Petrinet.to_string(net))
+
+let mat = Petrinet.to_matrix(net)
+print(mat.to_string())
+
+let mark = Marking.next_place(Nat.n(1), Marking.next_place(Nat.n(2), Marking.next_place(Nat.n(3), Marking.null())))
+
+print(Marking.to_string(mark))
+print(Marking.to_array(mark))
+
+let res = Marking.has_enough(mark, Integer.n(2), Nat.n(0))
+
+print(adtm.pprint(res))
+
+let r2 = adtm.eval(res)
+
+print(r2)
