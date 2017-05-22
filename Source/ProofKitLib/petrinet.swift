91b6e35dcc65d7d7cfae5c7f45d0bb196f069f84
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-23 01:52:40 +0200

    Added is_triggerable operator for petri nets

--- Source/ProofKitLib/petrinet.swift
+++ Source/ProofKitLib/petrinet.swift
@@ -93,6 +93,36 @@ public class Petrinet : ADT {
       super.init("petrinet")
       self.add_generator("null", Petrinet.null)
       self.add_generator("add_edge", Petrinet.add_edge, arity: 4)
+      self.add_operator("is_triggerable", Petrinet.is_triggerable, [
+        //is_triggerable(null, t, m) => True
+        Rule(Petrinet.is_triggerable(Petrinet.null(), Variable(named: "t"), Variable(named: "m")),
+          Boolean.True()
+        ),
+
+        //is_triggerable(add_edge(p, t, w, x), t, m) => has_enough(m, w, p) and is_triggerable(x, t, m)
+        Rule(
+          Petrinet.is_triggerable(
+            Petrinet.add_edge(Variable(named: "p"), Variable(named: "t"), Variable(named: "w"), Variable(named: "x")),
+            Variable(named: "t"),
+            Variable(named: "m")
+          ),
+          Boolean.and(
+            Marking.has_enough(Variable(named: "m"), Variable(named: "w"), Variable(named: "p")),
+            Petrinet.is_triggerable(Variable(named: "x"), Variable(named: "t"), Variable(named: "m"))
+            )
+        ),
+
+        //is_triggerable(add_edge(p, t1, w, x), t2, m) => has_enough(m, w, p) and is_triggerable(x, t, m)
+        Rule(
+          Petrinet.is_triggerable(
+            Petrinet.add_edge(Variable(named: "p"), Variable(named: "t1"), Variable(named: "w"), Variable(named: "x")),
+            Variable(named: "t2"),
+            Variable(named: "m")
+          ),
+            Petrinet.is_triggerable(Variable(named: "x"), Variable(named: "t2"), Variable(named: "m"))
+        )
+        ]
+      )
     }
 
     public static func null(_:Term...) -> Term{
@@ -141,5 +171,9 @@ public class Petrinet : ADT {
       }
     }
     return ""
+  }
+
+  public static func is_triggerable(_ operands: Term...) -> Term{
+    return Operator.n("is_triggerable", operands[0], operands[1], operands[2])
   }
 }
