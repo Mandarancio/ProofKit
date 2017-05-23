d84cdd336a51eb59b19785846be2ccd40a5de204
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-23 14:53:36 +0200

    Better Example

--- Source/ProofKitDemo2/main.swift
+++ Source/ProofKitDemo2/main.swift
@@ -1,14 +1,14 @@
 import LogicKit
 import ProofKitLib
 let adtm = ADTManager.instance()
-let a = Integer.n(5)
+/*let a = Integer.n(5)
 let b = Integer.n(3)
 let c = Integer.add(a,b)
 print("\(adtm.pprint(a)) + \(adtm.pprint(b))")
 print(adtm.pprint(c))
 let r = adtm.eval(c)
 print(adtm.pprint(r))
-print(r)
+print(r)*/
 
 /*
 
@@ -39,15 +39,15 @@ print(Petrinet.to_string(net))
 let mat = Petrinet.to_matrix(net)
 print(mat.to_string())
 
-let mark = Marking.next_place(Nat.n(1), Marking.next_place(Nat.n(2), Marking.next_place(Nat.n(3), Marking.null())))
+let mark = Marking.next_place(Nat.n(0), Marking.next_place(Nat.n(1), Marking.next_place(Nat.n(0), Marking.null())))
 
 print(Marking.to_string(mark))
 print(Marking.to_array(mark))
 
-let res = Marking.has_enough(mark, Integer.n(2), Nat.n(0))
+let res = adtm.eval(Marking.has_enough(mark, Integer.n(1), Nat.n(1)))
 
-print(adtm.pprint(res))
+print(res)
 
-let r2 = adtm.eval(res)
+let res2 = adtm.eval(Petrinet.is_triggerable(net, Nat.n(1), mark))
 
-print(r2)
+print(res2)
