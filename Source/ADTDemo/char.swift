7a6d6dfa5f580c46d4848fcf0ee26375f80be4ca
Martino Ferrari <manda.mgf@gmail.com>
2017-06-05 12:20:53 +0200

    Cleaner example

---
+++ Source/ADTDemo/char.swift
@@ -0,0 +1,56 @@
+import LogicKit
+import ProofKitLib
+
+// A Simple custom ADT: Char
+class Char: ADT{
+  // ADT Constructor
+  public init(){
+    super.init("char") // name of your adt
+    // add your generators with its arity (by default 0)
+    self.add_generator("a", Char.a)
+    self.add_generator("b", Char.b)
+    self.add_generator("c", Char.c)
+    // .....
+
+    // add your operators, and its axioms and arity (by default 2)
+    self.add_operator("c==",Char.eq, [
+      Rule(Char.eq(Variable(named: "x"),Variable(named: "x")), Boolean.True()),
+      Rule(Char.eq(Variable(named: "x"),Variable(named: "y")), Boolean.False())
+    ], arity:2)
+  }
+  // generator functions always (Term...)->Term
+  static public func a(_ :Term...)-> Term{
+    return Value<String>("a")
+  }
+  static public func b(_ :Term...)-> Term{
+    return Value<String>("b")
+  }
+  static public func c(_ :Term...)-> Term{
+    return Value<String>("c")
+  }
+  // ....
+
+  // Simple operator function (alyaws (Term...)->Term)
+  static public func eq(_ terms: Term...)->Term{
+    return Operator.n("c==",terms[0], terms[1])
+  }
+
+  // Goal to know if  a term belong to this adt (Goal result)
+  public class override func belong(_ term: Term ) -> Goal {
+    return term === Char.a() || term === Char.b() || term === Char.c()
+  }
+
+  // Simple check to check if a term is of this adt type (Boolean result)
+  public override func check(_ term: Term) -> Bool{
+    return term.equals(Char.a()) || term.equals(Char.b()) || term.equals(Char.c())
+  }
+
+  // Print nicely your term
+  public override func pprint(_ term: Term) -> String{
+    if let v = (term as? Value<String>){
+      return v.wrapped
+    }
+    return "?"
+  }
+
+}
--- Source/ADTDemo/main.swift
+++ Source/ADTDemo/main.swift
@@ -1,59 +1,21 @@
 import LogicKit
 import ProofKitLib
 
-class Char: ADT{
-  public init(){
-    super.init("char")
-    self.add_generator("a", Char.a)
-    self.add_generator("b", Char.b)
-    self.add_generator("c", Char.c)
-    // .....
-    self.add_operator("c==",Char.eq, [
-      Rule(Char.eq(Variable(named: "x"),Variable(named: "x")), Boolean.True()),
-      Rule(Char.eq(Variable(named: "x"),Variable(named: "y")), Boolean.False())
-    ], arity:2)
-  }
-
-  static public func a(_ :Term...)-> Term{
-    return Value<String>("a")
-  }
-  static public func b(_ :Term...)-> Term{
-    return Value<String>("b")
-  }
-  static public func c(_ :Term...)-> Term{
-    return Value<String>("c")
-  }
-  // ....
-
-  static public func eq(_ terms: Term...)->Term{
-    return Operator.n("c==",terms[0], terms[1])
-  }
-
-  public class override func belong(_ term: Term ) -> Goal {
-    return term === Char.a() || term === Char.b() || term === Char.c()
-  }
-
-  public override func pprint(_ term: Term) -> String{
-    if let v = (term as? Value<String>){
-      return v.wrapped
-    }
-    return "?"
-  }
-
-  public override func check(_ term: Term) -> Bool{
-    return term.equals(Char.a()) || term.equals(Char.b()) || term.equals(Char.c())
-  }
-}
-
+// How to use your new ADT?
+// Simple: you need the instance of the ADTManager class
 var adtm = ADTManager.instance()
+// Then you add your new adt to the manager
 adtm["char"] = Char()
+// now you can use it!
 
+// Example:
 let a = Char.a()
 let b = Char.b()
 var op = Char.eq(a, b)
 var r = adtm.eval(op)
 print("\(adtm.pprint(op)) => \(adtm.pprint(r))")
 
+// Counter-Example:
 op = Char.eq(a, a)
 r = adtm.eval(op)
 print("\(adtm.pprint(op)) => \(adtm.pprint(r))")
