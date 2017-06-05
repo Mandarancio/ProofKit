d7caa2d054f00d0f17140edc21de1fe925c684af
Florian Cabot <florian.cabot@etu.unige.ch>
2017-06-05 23:54:30 +0200

    Recreated Petrinet Demo

--- Source/PetrinetDemo/main.swift
+++ Source/PetrinetDemo/main.swift
@@ -1,28 +1,59 @@
 import LogicKit
 import ProofKitLib
+import PetrinetLib
 
+var adtm = ADTManager.instance()
+adtm["petrinet"] = Petrinet()
+adtm["marking"] = Marking()
 /*
+  [.] places
+  |.| transitions
+  -w->  edge with weight w
 
-[.] places
-|.| transitions
--w->  edge with weight w
+  this example is this net :
+ [0]-1->|0|<-1-[1]-1->|1|-2->[2]
+    <-2-
+ */
+
+ print("\n--------PETRINET---------\n")
+
+ let net = Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(1),
+           Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(-2),
+           Petrinet.add_edge(Nat.n(1), Nat.n(0), Integer.n(1),
+           Petrinet.add_edge(Nat.n(1), Nat.n(1), Integer.n(1),
+           Petrinet.add_edge(Nat.n(2), Nat.n(1), Integer.n(-2),
+           Petrinet.null()
+           )))))
+
+ print(Petrinet.to_string(net))
+
+ let mat = Petrinet.to_matrix(net)
+ print(mat.to_string())
+
+ let mark = Marking.next_place(Nat.n(0), Marking.next_place(Nat.n(1), Marking.next_place(Nat.n(0), Marking.null())))
+
+ print(Marking.to_string(mark))
+ print(Marking.to_array(mark))
+
+ let res = adtm.eval(Marking.has_enough(mark, Integer.n(1), Nat.n(1)))
+
+ print(res)
+
+ let res2 = adtm.eval(Petrinet.is_triggerable(net, Nat.n(1), mark))
+
+ print(res2)
 
 
-this example is this net :
+ print("\n ------ FARKAS ------ \n")
 
-[0]-1->|0|<-1-[1]-1->|1|-2->[2]
-   <-2-
+ let dynMat = DynamicMatrix(
+           [
+             [-1, 1, 1, -1],
+             [1, -1, -1, 1],
+             [0, 0, 1, 0],
+             [1, 0, 0, -1],
+             [-1, 0, 0, 1]
+           ])
 
-*/
-//let adtman_foo = ADTManager.instance()
+ print(dynMat.get_p_invariants())
 
-/*let net = Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(1),
-          Petrinet.add_edge(Nat.n(0), Nat.n(0), Integer.n(-2),
-          Petrinet.add_edge(Nat.n(1), Nat.n(0), Integer.n(1),
-          Petrinet.add_edge(Nat.n(1), Nat.n(1), Integer.n(1),
-          Petrinet.add_edge(Nat.n(2), Nat.n(1), Integer.n(-2),
-          Petrinet.null()
-          )))))
-
-print(Petrinet.to_string(net))
-print(Petrinet.to_matrix(net).to_string())*/
