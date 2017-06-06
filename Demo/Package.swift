58128c5fcec00aef23ed2b2b2587e12987de1c27
Martino Ferrari <manda.mgf@gmail.com>
2017-06-06 18:07:25 +0200

    Cleaned repo

--- Demo/Package.swift
+++ Demo/Package.swift
@@ -7,12 +7,11 @@ let package = Package(
  		  Target(name: "ProofKitDemo"),
  		  Target(name: "EqProofDemo"),
       Target(name: "ADTDemo"),
-      Target(name: "PetrinetDemo"),
     ],
     dependencies: [
         .Package(url: "https://github.com/kyouko-taiga/LogicKit",
                  majorVersion: 0),
-        .Package(url: "https://github.com/Dexter9313/ProofKit.git",
+        .Package(url: "https://github.com/Mandarancio/ProofKit",
                  majorVersion: 0),
     ]
 )
--- Package.swift
+++ Package.swift
@@ -2,10 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "ProofKit",
-    targets: [
-   	  Target(name: "ProofKitLib"),
-      Target(name: "PetrinetLib", dependencies: ["ProofKitLib"]),
-    ],
     dependencies: [
         .Package(url: "https://github.com/kyouko-taiga/LogicKit",
                  majorVersion: 0),
