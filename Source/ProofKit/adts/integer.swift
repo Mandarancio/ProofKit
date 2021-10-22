6bd260400741bda69ade28bac7ffe1880513aecc
damien.morard <damien.morard@unige.ch>
2021-10-22 12:13:50 +0200

    Remove the old test file

---
+++ Source/ProofKit/adts/integer.swift
@@ -0,0 +1,8 @@
+//
+//  integer.swift
+//  ProofKit
+//
+//  Created by Damien Morard on 22.10.21.
+//
+
+import Foundation
---
+++ Source/ProofKit/adts/natural.swift
@@ -0,0 +1,8 @@
+//
+//  natural.swift
+//  ProofKit
+//
+//  Created by Damien Morard on 22.10.21.
+//
+
+import Foundation
